945cd8fa1613adce8b7e82a56da44b1ad87ff891
Vladlena <strausik.vlada@gmail.com>
2026-04-24 18:26:07 +0300

    promise realised

---
+++ promise.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const array = [1, 2, 3];
+
+const asyncMapPromise = (x) => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(x * 2), 1000);
+  });
+};
+
+const asyncMap = (array, element, done) => {
+  return new Promise((resolve, reject) => {
+    if (array.length === 0) return resolve([]);
+
+    const results = [];
+    let completed = 0;
+    let isFinished = false;
+
+    array.forEach((item, index) => {
+      element(item)
+        .then((result) => {
+          if (isFinished) return;
+
+          results[index] = result;
+          completed++;
+
+          if (completed === array.length) {
+            isFinished = true;
+            resolve(results);
+          }
+        })
+        .catch((error) => {
+          if (!isFinished) {
+            isFinished = true;
+            reject(error);
+          }
+        });
+    });
+  });
+};
+
+asyncMap(array, asyncMapPromise)
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error.message));
